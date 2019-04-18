540c583a43ae3d75ba5b2dee6ce206d8f4102076
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-04-18 21:27:32 +0600

    added folder and the class files

---
+++ bin/main.rb
@@ -0,0 +1,10 @@
+require 'D:\Microverse\Ruby\Tic-tac-toe\lib\player.rb'  
+def tic_tac_toe
+    Print "Player name who wish to choose X :"
+    player1 = gets.chomp    
+    Print "Player name who wish to choose O :"
+    player2 = gets.chomp
+
+end
+
+tic_tac_toe
---
+++ lib/player.rb
@@ -0,0 +1,9 @@
+class Player
+  attr_reader : name, icon
+    def method_name(name, icon)
+      @name = name
+      @icon = icon     
+      @player_move = []
+    end
+    
+end
